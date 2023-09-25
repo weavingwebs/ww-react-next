fa1ead8ae7332b6996bd9f142ad1d3cb84dc272d
chris <chris@weavingwebs.co.uk>
2023-09-25 14:27:16 +0100

    docs: configure storybook to be Docs Mode

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,13 +5,15 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-docs',
   ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: true,
+    docsMode: true,
   },
 };
 export default config;
