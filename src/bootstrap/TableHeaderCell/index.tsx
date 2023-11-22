ef3036d4a5dc53fc159ace3c627b8a3602cc9901
Raf Sz <raf.sz@icloud.com>
2023-11-22 09:43:37 +0000

    feat: add <Table/> and <TableHeaderCell/> components

---
+++ src/bootstrap/TableHeaderCell/index.tsx
@@ -0,0 +1,14 @@
+import { FC, ThHTMLAttributes } from 'react';
+import clsx from 'clsx';
+
+type HeaderCellProps = Omit<ThHTMLAttributes<HTMLTableCellElement>, 'scope'>;
+
+export const TableHeaderCell: FC<HeaderCellProps> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <th scope="col" className={clsx(className)} {...props}>
+    {children}
+  </th>
+);
