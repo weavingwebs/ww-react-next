c3297b7e43e4215c501cec7f98596c4b5c7e7851
Raf Sz. <raf@weavingwebs.co.uk>
2024-09-18 10:03:17 +0100

    feat: bring back boostrap exports (NextLinkButton)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-// @todo Bring back once ww-react has been updated.
-// export * from './bootstrap';
+export * from './bootstrap';
 export * from './components';
 export * from './hooks';
 export * from './lib';
